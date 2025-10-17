6a154e2080aecbcfdc4c6516fdcf89d90516ed7e
Xiker <xikerx999@gmail.com>
2025-10-17 23:51:20 +0200

    CHORE: Commit inicial Actividad1 Unidad 2

---
+++ UD2/ACT1-3/ACT1/src/Actividad1.java
@@ -0,0 +1,24 @@
+/**
+ * <h2>Clase Actividad1</h2>
+ * Clase principal para ejecutar dos hilos que imprimen "Primero" y "Segundo" 20 veces.
+ * Cada hilo es una instancia de la clase Hilo.
+ *
+ * @author Xiker
+ * @version 1.0
+ * @since 17-10-2025
+ */
+public class Actividad1 {
+    /**
+     * Método principal que crea y ejecuta los hilos.
+     * @param args Argumentos de línea de comandos (no se usan)
+     */
+    public static void main(String[] args){
+        Hilo primero = new Hilo("Primero", 20);
+        Hilo segundo = new Hilo("Segundo", 20);
+
+        primero.start();
+        segundo.start();
+
+        System.out.println("Fin programa");
+    }
+}
---
+++ UD2/ACT1-3/ACT1/src/Hilo.java
@@ -0,0 +1,41 @@
+/**
+ * <h2>Clase Hilo</h2>
+ * Esta clase representa un hilo que imprime un nombre y un número de repeticiones.
+ * Se utiliza en la Actividad 1 de la Unidad 2 para reemplazar las clases Primero y Segundo.
+ *
+ * @author Xiker
+ * @version 1.0
+ * @since 17-10-2025
+ */
+public class Hilo extends Thread {
+    /**
+     * Nombre que imprime el hilo.
+     */
+    private String nombre;
+
+    /**
+     * Número de repeticiones que hará el hilo.
+     */
+    private int repeticiones;
+
+    /**
+     * Constructor del hilo.
+     * @param nombre Nombre a imprimir
+     * @param repeticiones Número de veces que imprimirá
+     */
+    public Hilo(String nombre, int repeticiones){
+        this.nombre = nombre;
+        this.repeticiones = repeticiones;
+    }
+
+    /**
+     * Método que se ejecuta al iniciar el hilo.
+     * Imprime el nombre seguido del contador de 1 a repeticiones.
+     */
+    @Override
+    public void run() {
+        for(int i = 1; i <= repeticiones; i++){
+            System.out.println(nombre + " " + i);
+        }
+    }
+}
